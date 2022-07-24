3f010dbdff003d00bd675771d6c64a76065e2f51
M-Chan <mantingchan2@gmail.com>
2022-07-24 21:48:11 +0100

    commenting

--- app/app.js
+++ app/app.js
@@ -7,8 +7,10 @@ let card = document.querySelectorAll(".card");
 let font1 = document.querySelectorAll(".font1");
 let font2 = document.querySelectorAll(".font2");
 
+
+// the function below 'toggles darkmode' by switching the colours between a light theme and a dark theme
 function toggleDarkMode() {
-    if(screen.classList.contains("containerLight")) {
+    if(screen.classList.contains("containerLight")) { // if in lightmode then switch to dark mode
         console.log("In Light Mode");
         console.log("In Light Mode switching to Dark Mode");
         screen.classList.remove("containerLight");
@@ -16,12 +18,12 @@ function toggleDarkMode() {
         screen.classList.add("containerDark");
         banner.classList.add("bannerDark");
 
-        card.forEach(item => {
+        card.forEach(item => { // change the colour of the cards from grey to dark desaturated blue
             item.classList.remove("cardLight");
             item.classList.add("cardDark");
         })
 
-        font1.forEach(item => {
+        font1.forEach(item => { // change the very dark blue font to white
             item.classList.remove("fontLight1");
             item.classList.add("fontDark1");
         })
@@ -31,7 +33,8 @@ function toggleDarkMode() {
             item.classList.add("fontDark2");
         })
     }
-    else if (screen.classList.contains("containerDark")) {
+
+    else if (screen.classList.contains("containerDark")) { // if in darkmode then switch to light mode
         console.log("In Dark Mode");
         console.log("In Dark Mode switching to Light Mode");
         screen.classList.remove("containerDark");
@@ -39,12 +42,12 @@ function toggleDarkMode() {
         screen.classList.add("containerLight");
         banner.classList.add("bannerLight");
 
-        card.forEach(item => {
+        card.forEach(item => { // change the colour of the cards from dark desaturated blue to grey
             item.classList.remove("cardDark");
             item.classList.add("cardLight");
         })
 
-        font1.forEach(item => {
+        font1.forEach(item => { // change the white font to very dark blue
             item.classList.remove("fontDark1");
             item.classList.add("fontLight1");
         })
@@ -54,7 +57,8 @@ function toggleDarkMode() {
             item.classList.add("fontLight2");
         })
     }
-    else {
+
+    else { // modetype not found
         console.log("Error");
     }
 }
