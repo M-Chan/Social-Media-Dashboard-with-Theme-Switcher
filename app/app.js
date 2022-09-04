080dbf13c8be55154d45aa06242afdccf26d5ccb
M-Chan <mantingchan2@gmail.com>
2022-09-04 18:19:37 +0100

    refactoring theme code to condense

--- app/app.js
+++ app/app.js
@@ -16,75 +16,46 @@ toggleDot.classList.remove("toggleDotInitial");
 function toggleDarkMode() {
     if(banner.classList.contains("bannerLight")) { // if in lightmode then switch to dark mode
         document.body.style.background = "hsl(230, 17%, 14%)";
-
-        console.log("In Light Mode");
-        console.log("In Light Mode switching to Dark Mode");
-        banner.classList.remove("bannerLight");
-        toggleButton.classList.remove("toggleLight");
-        toggleDot.classList.remove("toggleDotLight");
-        attribution.classList.remove("attributionLight");
-        banner.classList.add("bannerDark");
-        toggleButton.classList.add("toggleDark");
-        toggleDot.classList.add("toggleDotDark");
-        attribution.classList.add("attributionDark");
-
-        card.forEach(item => { // change the colour of the cards from grey to dark desaturated blue
-            item.classList.remove("cardLight");
-            item.classList.add("cardDark");
-        })
-
-        font1.forEach(item => { // change the very dark blue font to white
-            item.classList.remove("fontLight1");
-            item.classList.add("fontDark1");
-        })
-
-        font2.forEach(item => {
-            item.classList.remove("fontLight2");
-            item.classList.add("fontDark2");
-        })
-
-        font5.forEach(item => {
-            item.classList.remove("fontLight5");
-            item.classList.add("fontDark5");
-        })
     }
 
     else if (banner.classList.contains("bannerDark")) { // if in darkmode then switch to light mode
         document.body.style.background = "hsl(0, 0%, 100%)";
-
-        console.log("In Dark Mode");
-        console.log("In Dark Mode switching to Light Mode");
-        banner.classList.remove("bannerDark");
-        toggleButton.classList.remove("toggleDark");
-        toggleDot.classList.remove("toggleDotDark");
-        attribution.classList.remove("attributionDark");
-        banner.classList.add("bannerLight");
-        toggleButton.classList.add("toggleLight");
-        toggleDot.classList.add("toggleDotLight");
-        attribution.classList.add("attributionLight");
-
-        card.forEach(item => { // change the colour of the cards from dark desaturated blue to grey
-            item.classList.remove("cardDark");
-            item.classList.add("cardLight");
-        })
-
-        font1.forEach(item => { // change the white font to very dark blue
-            item.classList.remove("fontDark1");
-            item.classList.add("fontLight1");
-        })
-
-        font2.forEach(item => {
-            item.classList.remove("fontDark2");
-            item.classList.add("fontLight2");
-        })
-
-        font5.forEach(item => {
-            item.classList.remove("fontDark5");
-            item.classList.add("fontLight5");
-        })
     }
 
     else { // modetype not found
         console.log("Error");
     }
+
+    banner.classList.toggle("bannerLight");
+    banner.classList.toggle("bannerDark");
+
+    toggleButton.classList.toggle("toggleLight");
+    toggleButton.classList.toggle("toggleDark");
+
+    toggleDot.classList.toggle("toggleDotLight");
+    toggleDot.classList.toggle("toggleDotDark");
+
+    attribution.classList.toggle("attributionLight");
+    attribution.classList.toggle("attributionDark");
+
+
+    card.forEach(item => { // change the colour of the cards
+        item.classList.toggle("cardLight");
+        item.classList.toggle("cardDark");
+    })
+
+    font1.forEach(item => {
+        item.classList.toggle("fontLight1");
+        item.classList.toggle("fontDark1");
+    })
+
+    font2.forEach(item => {
+        item.classList.toggle("fontLight2");
+        item.classList.toggle("fontDark2");
+    })
+
+    font5.forEach(item => {
+        item.classList.toggle("fontLight5");
+        item.classList.toggle("fontDark5");
+    })
 }
